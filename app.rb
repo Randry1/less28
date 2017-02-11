b8041dfafd13e4c665b28c3e2f117ea35d407e41
Randry1 <revinandry@gmail.com>
2017-02-11 20:57:02 +0700

    add table Post

--- app.rb
+++ app.rb
@@ -6,12 +6,22 @@ require 'sinatra/reloader'
 require 'sqlite3'
 
 def init_db
-  db = SQLite3::Database.new 'leprosorium.db'
-  db.results_as_hash = true
+  @db = SQLite3::Database.new 'leprosorium.db'
+  @db.results_as_hash = true
 end
 
 before do
+  init_db
+end
 
+configure do
+  init_db
+  @db.execute 'CREATE TABLE IF NOT EXISTS "Posts"
+                (
+                   "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+                    "create_date" DATE,
+                    "content" TEXT
+                );'
 end
 
 get '/' do
